2bf2b3d646cdc2a890723d3f363392be4a925407
Faizan Akhter <faizan.ac@yahoo.com>
2024-03-08 03:23:52 +0100

    references citations issue fixed

--- backend/services/api.py
+++ backend/services/api.py
@@ -10,9 +10,11 @@ async def fetch_references_citation(paperId:str):
     REFERENCES_URL=f'{SEMANTIC_SCHOLAR_BASEURL}/{paperId}/references?fields={FIELDS}&offset=0&limit=10'
     CITATIONS_URL=f'{SEMANTIC_SCHOLAR_BASEURL}/{paperId}/citations?fields={FIELDS}&offset=0&limit=10'
     referencesResp = requests.get(REFERENCES_URL,headers={'x-api-key':'IwS2rC34Rm2PvIG17qnhv8IUIm2z4dPv967WR9pF'}).json()
-    citationsResp = requests.get(CITATIONS_URL).json()
-
-    simplified_paper_obj = [{**paper["citedPaper"], "parentId": paperId} for paper in referencesResp["data"]] + [{**paper["citingPaper"],"parentId":paperId} for paper in citationsResp["data"]]
+    citationsResp = requests.get(CITATIONS_URL,headers={'x-api-key':'IwS2rC34Rm2PvIG17qnhv8IUIm2z4dPv967WR9pF'}).json()
+    print(citationsResp)
+    simplified_paper_obj=[]
+    if(referencesResp["data"] is not None and citationsResp["data"] is not None):
+        simplified_paper_obj = [{**paper["citedPaper"], "parentId": paperId} for paper in referencesResp["data"]] + [{**paper["citingPaper"],"parentId":paperId} for paper in citationsResp["data"]]
     return simplified_paper_obj
 
 async def get_search_result(query:str):
