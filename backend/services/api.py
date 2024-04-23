8207f808d05191649b270022fd66a4708e298da7
Faizan Akhter <faizan.ac@yahoo.com>
2024-04-23 20:42:59 +0500

    References and citations field removed temporarily

--- backend/services/api.py
+++ backend/services/api.py
@@ -3,6 +3,8 @@ import requests
 
 SEMANTIC_SCHOLAR_BASEURL = "https://api.semanticscholar.org/graph/v1/paper"
 FIELDS = "title,publicationDate,journal,referenceCount,citationCount,abstract,venue"
+REFENRENCES_CITATIONS_FIELD=",references,citations"
+REFENRENCES_CITATIONS_FIELD=""
 LIMIT, OFFSET = 20, 0
 
 
@@ -17,7 +19,7 @@ async def fetch_references_citation(paperId:str):
     return simplified_paper_obj
 
 async def get_search_result(query:str):
-        SEARCH_URL=f'{SEMANTIC_SCHOLAR_BASEURL}/search?fields={FIELDS},references,citations&query={query.replace(" ", "%20")}&limit={LIMIT}&offset={OFFSET}'
+        SEARCH_URL=f'{SEMANTIC_SCHOLAR_BASEURL}/search?fields={FIELDS+REFENRENCES_CITATIONS_FIELD}&query={query.replace(" ", "%20")}&limit={LIMIT}&offset={OFFSET}'
         response = requests.get(SEARCH_URL,headers={'x-api-key':'IwS2rC34Rm2PvIG17qnhv8IUIm2z4dPv967WR9pF'})
         print(f'semantic scholar headers: {response.headers}')
         return response
